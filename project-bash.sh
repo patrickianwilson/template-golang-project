287fd1a11c1ba04604651b611885138ef17a087e
Patrick Wilson <pwilson@smarttech.com>
2016-07-22 11:15:52 -0700

    added better instructions for how to run your golang project

--- project-bash.sh
+++ project-bash.sh
@@ -6,8 +6,10 @@ export PATH=$PATH:$GOPATH/bin
 echo "GOPATH set to: $GOPATH"
 
 echo "Some Helpful Go Commands"
-
-echo "go install github.com/<user>/hello"
+echo "To Build and Run"
+echo "go build github.com/patrickianwilson/hello && ./hello" 
+echo ""
+echo "additional Go Tools"
 echo "go get <package name>"
 echo "Go Debugging: https://github.com/mailgun/godebug"
 echo "godebug "
