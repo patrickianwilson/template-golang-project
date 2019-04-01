ea7fcfbf526b623bf0aac07e917d2f788d1dc47f
pwilson <pwilson@inquestdevops.com>
2019-04-01 07:32:53 -0700

    creating an openapi codegen library based on the existing swagger lib.
    This template should be used to generate clients for openapi services.

--- refresh-api.sh
+++ refresh-api.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-export WS_URL=http://cassius.wilsonsinquest.com
+export WS_URL=http://modules.wilsonsinquest.com
 export GROUP_ID=mygroup
 export SERVICE_ID=myservice
 
 export GOPATH=`pwd`
 
 mkdir -p ${GOPATH}/build/tmp
-
-if [ ! -f ${GOPATH}/build/tmp/codegen.jar ]; then
-    wget -O ${GOPATH}/build/tmp/codegen.jar http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.2.3/swagger-codegen-cli-2.2.3.jar
+mkdir -p ${GOPATH}src/clientsdk/moduleservice
+if [[ ! -f ${GOPATH}/build/tmp/openapi-codegen.jar ]]; then
+    wget -O ${GOPATH}/build/tmp/openapi-codegen.jar http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/3.3.4/openapi-generator-cli-3.3.4.jar
 fi
 
-java -jar ${GOPATH}/build/tmp/codegen.jar generate -i ${WS_URL}/swagger.yaml -l go -o ${GOPATH}/src/github.com/$GROUP_ID/$SERVICE_ID -c ${GOPATH}/config/codegen-go-config.json
-
+java -jar ${GOPATH}/build/tmp/openapi-codegen.jar generate\
+ --input-spec ${WS_URL}/openapi.yaml\
+ --generator-name go\
+ --output ${GOPATH}/src/github.com/$GROUP_ID/$SERVICE_ID \
+ --config ${GOPATH}/config/codegen-go-config.json
