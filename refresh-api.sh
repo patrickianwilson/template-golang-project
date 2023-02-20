3573267b80cbeddb97468671c59fbd28d3ccd52d
pwilson <pwilson@inquestdevops.com>
2023-02-20 11:09:00 -0800

    updating import script to latest open api

--- refresh-api.sh
+++ refresh-api.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-export WS_URL=http://modules.wilsonsinquest.com
-export GROUP_ID=mygroup
-export SERVICE_ID=myservice
+#Change Me!!
+export WS_URL=http://service-srv.inquestdevops.com
+export GROUP_ID=inquestdevops.com
+export SERVICE_ID=changme
 
 export GOPATH=`pwd`
 
 mkdir -p ${GOPATH}/build/tmp
-mkdir -p ${GOPATH}src/clientsdk/moduleservice
+mkdir -p ${GOPATH}src/${GROUP_ID}/${SERVICE_ID}
 if [[ ! -f ${GOPATH}/build/tmp/openapi-codegen.jar ]]; then
-    wget -O ${GOPATH}/build/tmp/openapi-codegen.jar http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/3.3.4/openapi-generator-cli-3.3.4.jar
+    wget -O ${GOPATH}/build/tmp/openapi-codegen.jar https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/6.2.1/openapi-generator-cli-6.2.1.jar
 fi
 
 java -jar ${GOPATH}/build/tmp/openapi-codegen.jar generate\
  --input-spec ${WS_URL}/openapi.yaml\
  --generator-name go\
- --output ${GOPATH}/src/github.com/$GROUP_ID/$SERVICE_ID \
- --config ${GOPATH}/config/codegen-go-config.json
+ --output ${GOPATH}/src/${GROUP_ID}/${SERVICE_ID} \
+ --config ${GOPATH}/config/codegen-go-config.json \
+ -p enumClassPrefix=true\
+ --skip-validate-spec
