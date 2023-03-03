34af01838357dfa8206885e628b09e3f3f44021b
pwilson <pwilson@inquestdevops.com>
2023-03-03 08:05:57 -0800

    Separating build from release for teamcity pipelines

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,9 +1,15 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.dockerSupport
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.GradleBuildStep
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.buildFeatures.dockerSupport
+import jetbrains.buildServer.configs.kotlin.buildSteps.DockerCommandStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.dockerCommand
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.buildSteps.GradleBuildStep
+import jetbrains.buildServer.configs.kotlin.buildSteps.ScriptBuildStep
+
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.triggers.finishBuildTrigger
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -36,20 +42,20 @@ object BuildConstants{
 
 project {
 
-    buildType(BuildAndInternalPublish)
+    buildType(Build)
+    buildType(Release)
 }
 
-object BuildAndInternalPublish : BuildType({
-    name = "Build and Internal Publish"
-
-
+object Build : BuildType({
+    name = "Build"
+    artifactRules = "build/**/* => build_artifacts"
     vcs {
         root(DslContext.settingsRoot)
     }
 
     steps {
         gradle {
-            tasks = "clean release"
+            tasks = "release"
             useGradleWrapper = false
             dockerImagePlatform = GradleBuildStep.ImagePlatform.Linux
             dockerPull = true
@@ -75,6 +81,52 @@ object BuildAndInternalPublish : BuildType({
                     connectionId = "PROJECT_EXT_2"
                 }
                 displayName = "TeamCity"
+            }
+        }
+    }
+})
+
+object Release : BuildType({
+    name = "Release"
+    maxRunningBuilds = 1
+    type = BuildTypeSettings.Type.DEPLOYMENT
+    enablePersonalBuilds = false
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        gradle {
+            tasks = "publishAllPublicationsToMavenInternalRepository"
+            useGradleWrapper = false
+            dockerImagePlatform = GradleBuildStep.ImagePlatform.Linux
+            dockerImage = "inquest.registry.jetbrains.space/p/buildtools/buildimages/buildimage:latest"
+        }
+    }
+
+    triggers {
+        finishBuildTrigger {
+            buildType = "${Build.id}"
+            successfulOnly = true
+        }
+    }
+    features {
+        dockerSupport {
+            loginToRegistry = on {
+                dockerRegistryId = "PROJECT_EXT_5"
+            }
+        }
+    }
+
+    dependencies {
+        dependency(Build) {
+            snapshot {
+                onDependencyFailure = FailureAction.CANCEL
+                onDependencyCancel = FailureAction.CANCEL
+            }
+
+            artifacts {
+                artifactRules = "build_artifacts/**"
             }
         }
     }
